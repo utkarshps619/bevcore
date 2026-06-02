bf2ad7d10cbf39a5b5d67d92edad4d43982eb4b6
utkarshps619 <utkarshps619@gmail.com>
2026-06-02 14:11:09 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { PourCostDashboard } from './pages/PourCostDashboard';
 import VarianceDashboard from './pages/VarianceDashboard';
 import OpenItemCalculator from './pages/OpenItemCalculator';
 import { StockAdjustmentsPage } from './pages/StockAdjustmentsPage';
+import { MultiOutletDashboard } from './pages/MultiOutletDashboard';
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
@@ -60,6 +61,7 @@ function AppRoutes() {
         <Route path="pour-cost" element={<PourCostDashboard />} />
         <Route path="stock-adjustments" element={<StockAdjustmentsPage />} />
         <Route path="variance" element={<VarianceDashboard />} />
+        <Route path="multi-outlet" element={<MultiOutletDashboard />} />
         <Route path="open-item" element={<OpenItemCalculator />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
